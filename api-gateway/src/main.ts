12e302079eb40bdd98999dd5f3bc9277945a9e2e
IlliaMaluk <32estabes@gmail.com>
2025-03-31 13:40:41 +0300

    fix api-gateway

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.connectMicroservice({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://rabbitmq:5672'],
+      urls: [process.env.RABBITMQ_URL || 'amqp://rabbitmq:5672'],
       queue: 'api_gateway_queue',
       queueOptions: { durable: false },
     },
